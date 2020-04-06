44b2be59c9f4a6226e61afe5d7b2388c8df9112c
Babak <babak.mahjoub@gmail.com>
2020-04-06 07:39:50 +0000

    Change port server

--- app.js
+++ app.js
@@ -53,6 +53,6 @@ app.use(campgroundRoutes)
 
 
 
-app.listen ("3000", function(){
-	console.log("Server started on port 3000")
+app.listen (process.env.PORT || "3000", function(){
+	console.log("Server started")
 });
